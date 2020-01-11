248ac4754cdeab0beb4084ead91aa75aaadb406b
Paris Guillaume <guillaumejparis@gmail.com>
2020-01-12 00:07:24 +0100

    Fix linter

--- recipes/index.js
+++ recipes/index.js
@@ -1,1 +1,8 @@
-export default [{ name: "Pâte à pizza", filename: "pate-a-pizza" }, { name: "Petits Pains Alsaciens", filename: "petits-pains-alsaciens" }, { name: "Cookies moelleux aux pépites de chocolat", filename: "cookies-moelleux-aux-pepites-de-chocolat" }];
+export default [
+  { name: "Pâte à pizza", filename: "pate-a-pizza" },
+  { name: "Petits Pains Alsaciens", filename: "petits-pains-alsaciens" },
+  {
+    name: "Cookies moelleux aux pépites de chocolat",
+    filename: "cookies-moelleux-aux-pepites-de-chocolat"
+  }
+];
