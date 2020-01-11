d5df96f56f7982827ea724f98bfbd5b4071ac234
Guillaume <guillaumejparis@gmail.com>
2020-01-12 00:04:42 +0100

    Update list of recipes

--- recipes/index.js
+++ recipes/index.js
@@ -1,1 +1,1 @@
-export default [{ name: "Pâte à pizza", filename: "pizza" }];
+export default [{ name: "Pâte à pizza", filename: "pate-a-pizza" }, { name: "Petits Pains Alsaciens", filename: "petits-pains-alsaciens" }, { name: "Cookies moelleux aux pépites de chocolat", filename: "cookies-moelleux-aux-pepites-de-chocolat" }];
