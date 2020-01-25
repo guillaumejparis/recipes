f8a44bdde2772ae8b31d744201b6bbefdea0f4a4
Paris Guillaume <guillaumejparis@gmail.com>
2020-01-25 18:05:34 +0100

    Add pâte à crêpes recipe

--- recipes/index.js
+++ recipes/index.js
@@ -11,6 +11,7 @@ export default [
     name: "Cookies moelleux aux pépites de chocolat",
     filename: "cookies-moelleux-aux-pepites-de-chocolat"
   },
+  { name: "Pâte à crèpes", filename: "pate-a-crepes" },
   { name: "Pâte à pizza", filename: "pate-a-pizza" },
-  { name: "Petits Pains Alsaciens", filename: "petits-pains-alsaciens" },
+  { name: "Petits Pains Alsaciens", filename: "petits-pains-alsaciens" }
 ];
