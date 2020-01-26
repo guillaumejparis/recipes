be05260372a474e4bedc10db39312b2f28f613a3
Guillaume <guillaumejparis@gmail.com>
2020-01-26 12:50:13 +0100

    Add filet mignon aux champignons et marrons recipe (#10)
    * Update index.js
    
    * Add filet mignon aux champignons et marrons recipe

--- recipes/index.js
+++ recipes/index.js
@@ -11,6 +11,10 @@ export default [
     name: "Cookies moelleux aux pépites de chocolat",
     filename: "cookies-moelleux-aux-pepites-de-chocolat"
   },
+  {
+    name: "Filet mignon aux champignons et marrons",
+    filename: "filet-mignon-aux-champignons-et-marrons"
+  },
   { name: "Pâte à crèpes", filename: "pate-a-crepes" },
   { name: "Pâte à pizza", filename: "pate-a-pizza" },
   { name: "Petits Pains Alsaciens", filename: "petits-pains-alsaciens" }
