fd172fc81876b3041aa871872c857ba8cd10e16a
Guillaume Paris <guillaumejparis@gmail.com>
2020-04-24 18:21:16 +0200

    Add searching
    Add searching with fuse.js
    Little conf fix in github actions workflow
    App bar sass improvement

--- src/components/appBar/index.js
+++ src/components/appBar/index.js
@@ -1,52 +1,88 @@
 import { h } from 'preact';
+import { useContext, useState } from 'preact/hooks';
 
-import { useRecipesLocation } from 'components/app';
+import {
+  RecipesContext,
+  recipesFuse,
+  useRecipesLocation,
+} from 'components/app';
 import Text from 'components/text';
 
 import { useRecipe } from 'hooks/recipe';
 import { useScrolledFromTop } from 'hooks/scroll';
+
+import recipes from 'recipes';
 
 import { getSpacing } from 'theme/themeHelper';
 
 import './style.scss';
 
 const AppBar = () => {
+  const [, setRecipes] = useContext(RecipesContext);
   const [, setLocation] = useRecipesLocation();
   const [recipe] = useRecipe();
   const [scrolledFromTop] = useScrolledFromTop(getSpacing());
+  const [search, setSearch] = useState(null);
 
   return (
     <>
       <div styleName="ghost-container" />
       <div
-        styleName={`${
-          scrolledFromTop ? 'container-scrolled-from-top' : ''
-        } container`}
+        styleName={`container ${
+          scrolledFromTop ? 'container--scrolled-from-top' : ''
+        } ${search != null ? 'container--search' : ''}`}
       >
-        <div
-          onClick={() => setLocation('/')}
-          styleName={`${recipe ? 'left-recipe' : 'left-home'} left`}
-        >
-          {recipe ? (
-            <button styleName="button">
-              <i className="fas fa-home" styleName="home-icon" />
-            </button>
-          ) : (
-            <Text
-              color={scrolledFromTop ? 'white' : 'app-primary'}
-              variant="h5"
+        {search === null ? (
+          <>
+            <div styleName={`${recipe ? 'left-recipe' : 'left-home'} left`}>
+              {recipe ? (
+                <button onClick={() => setLocation('/')} styleName="button">
+                  <i className="fas fa-home" styleName="icon" />
+                </button>
+              ) : (
+                <Text
+                  color={scrolledFromTop ? 'white' : 'app-primary'}
+                  variant="h5"
+                >
+                  Recettes
+                </Text>
+              )}
+            </div>
+            {!recipe && (
+              <button onClick={() => setSearch('')} styleName="button">
+                <i className="fas fa-search" styleName="icon" />
+              </button>
+            )}
+          </>
+        ) : (
+          <form styleName="form">
+            <input
+              autoFocus
+              onInput={(event) => {
+                const { value } = event.target;
+
+                setSearch(value);
+                if (value) {
+                  setRecipes(recipesFuse.search(value));
+                } else {
+                  setRecipes(recipes);
+                }
+              }}
+              placeholder="Recherche"
+              styleName="input"
+              value={search}
+            />
+            <button
+              onClick={() => {
+                setSearch(null);
+                setRecipes(recipes);
+              }}
+              styleName="button"
             >
-              Recettes
-            </Text>
-          )}
-        </div>
-        <div styleName="right">
-          {!recipe && (
-            <button styleName="button">
-              <i className="fas fa-search" styleName="search-icon" />
+              <i className="fas fa-times" styleName="icon" />
             </button>
-          )}
-        </div>
+          </form>
+        )}
       </div>
     </>
   );
