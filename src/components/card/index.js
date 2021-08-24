69a6c5b703b129fdc84b58a926c1cddd8d64992e
Guillaume Paris <guillaumejparis@gmail.com>
2021-08-24 18:01:38 +0200

    Fix card

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -8,7 +8,7 @@ const Card = ({ className, onClick, tags, title }) => (
   <div styleName="container" className={className} onClick={onClick}>
     <div styleName="content">
       <Text color="app-primary" component="h2" variant="body1">
-        {tags[0]}
+        {tags?.[0]}
       </Text>
       <div className={styles.titleContainer}>
         <Text className={styles.title} component="h2" variant="body1">
